7da64cb55113c0aa21fdf83aec5916f5d4f8e0b0
Tulio <221194504+tulin404@users.noreply.github.com>
2025-12-24 17:36:09 -0300

    Theme logic finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,49 @@
+// THEME
 const sun = document.getElementById('sun');
 const moon = document.getElementById('moon');
 
-checkThemeIcons();
+window.addEventListener('load', checkTheme);
+
+function checkTheme() {
+    checkStorage();
+    checkThemeIcons();
+};
 
 function checkThemeIcons() {
-    if (document.documentElement.classList.contains('dark')) {
+    if (localStorage.getItem('theme') === 'dark' || isBrowserDark() && localStorage.getItem('theme') === null) {
         sun.classList.add('opacity-0', 'z-10', 'pointer-events-none');
         moon.classList.remove('opacity-0', 'z-10', 'pointer-events-none');
-    } else {
+    } else if (localStorage.getItem('theme') === 'light' || !isBrowserDark() && localStorage.getItem('theme') === null) {
         moon.classList.add('opacity-0', 'z-10', 'pointer-events-none');
         sun.classList.remove('opacity-0', 'z-10', 'pointer-events-none');
     };
+};
+
+function isBrowserDark() {
+    return (window.matchMedia('(prefers-color-scheme: dark)').matches)
 }
 
+function checkStorage() {
+    if (localStorage.getItem('theme') === null) {
+        if (isBrowserDark()) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        };
+    } else {
+        if (localStorage.getItem('theme') === 'dark') {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        };
+    };
+};
+
 sun.addEventListener('click', () => {
-    document.documentElement.classList.add('dark');
-    checkThemeIcons();
+    localStorage.setItem('theme', 'dark');
+    checkTheme();
 });
-
 moon.addEventListener('click', () => {
-    document.documentElement.classList.remove('dark');
-    checkThemeIcons();
+    localStorage.setItem('theme', 'light');
+    checkTheme();
 });
-
