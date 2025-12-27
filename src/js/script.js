985227f3200370fd70ca78c6f29a3bd6b00708fb
Tulio <221194504+tulin404@users.noreply.github.com>
2025-12-27 01:09:52 -0300

    Finish frontend of adding books

--- src/js/script.js
+++ src/js/script.js
@@ -53,6 +53,9 @@ moon.addEventListener('click', () => {
 const addBtn = document.getElementById('add-btn');
 const cancelBtn = document.getElementById('cancell-btn');
 const bookInput = document.getElementById('book-input-div');
+const titleInput = document.getElementById('title-input');
+const authorInput = document.getElementById('author-input');
+const pagesInput = document.getElementById('pages-input');
 
 addBtn.addEventListener('click', inputOpenClose);
 cancelBtn.addEventListener('click', inputOpenClose);
@@ -60,8 +63,70 @@ cancelBtn.addEventListener('click', inputOpenClose);
 function inputOpenClose() {
     bookInput.classList.toggle('scale-0');
     document.documentElement.classList.toggle('opacity-bg');
+    titleInput.value = '';
+    authorInput.value = '';
+    pagesInput.value = '';
 };
+
+function isValid(element) {
+    if (!element.checkValidity()) {
+        element.style.animation = 'invalid 0.3s forwards';
+        element.style.border = '2px solid red';
+    } else {
+        element.style.border = '';
+        element.style.animation = '';
+    }
+};
+
+titleInput.addEventListener('input', () => isValid(titleInput));
+authorInput.addEventListener('input', () => isValid(authorInput));
+pagesInput.addEventListener('input', () => isValid(pagesInput));
 
 // SUBMIT BOOK
 const subBtn = document.getElementById('sub-btn');
+const booksGrid = document.getElementById('books-grid');
 
+let booksArray = [];
+
+subBtn.addEventListener('click', subBook);
+
+function checkValidity() {
+    if (titleInput.checkValidity() && authorInput.checkValidity() && pagesInput.checkValidity()) {
+        return true;
+    } else {
+        return false
+    };
+};
+
+function subBook() {
+    if (checkValidity()) {
+    const title = titleInput.value;
+    const author = authorInput.value;
+    const pages = pagesInput.value;
+    const booksGrid = document.getElementById('books-grid');
+
+    const bookWrapper = document.createElement('div');
+    bookWrapper.classList.add('flex', 'flex-col', 'text-center', 'rounded-md', 'px-6', 'py-4', 'bg-primary', 'shadow-md', 'gap-4')
+    booksGrid.appendChild(bookWrapper);
+    bookWrapper.innerHTML = `
+    <div id="info-wrapper">
+        <div id="book-title" class="font-semibold text-2xl">${title}</div>
+        <div id="book-author">${author}</div>
+        <div id="book-pages" class="text-[15px]">${pages}</div>
+    </div>
+    <div id="btns-wrapper" class="text-xl flex flex-col items-center gap-2">
+            <button id="read-btn" class="w-[80%] bg-green-400 text-green-100 py-0.5 rounded-md">
+                Read
+            </button>
+            <button id="remove-btn" class="w-[80%] bg-red-400 text-red-100 py-0.5 rounded-md">
+                Remove
+            </button>
+    </div>`
+
+    inputOpenClose();
+    } else {
+        isValid(titleInput);
+        isValid(authorInput);
+        isValid(pagesInput);
+    };
+};
